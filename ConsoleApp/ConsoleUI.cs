ded105be4c6119e8ea693e593e6760836f7f1fc6
Marcus Nilsson <xarf93@hotmail.com>
2024-02-07 14:57:27 +0100

    Add project files.

---
+++ ConsoleApp/ConsoleUI.cs
@@ -0,0 +1,213 @@
+﻿using ConsoleApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class ConsoleUI
+    {
+        private readonly ProductService _productService;
+        private readonly CustomerService _customerService;
+        
+
+        public ConsoleUI(ProductService productService, CustomerService customerService)
+        {
+            _productService = productService;
+            _customerService = customerService;
+        }
+
+
+        //PRODUCTS
+        public void CreateProduct_UI()
+        {
+            Console.Clear();
+            Console.WriteLine("Create Product");
+
+            Console.Write("Product Title: ");
+            var title = Console.ReadLine()!;
+
+            Console.Write("Product Price: ");
+            decimal price = decimal.Parse(Console.ReadLine()!);
+
+            Console.Write("Product Category: ");
+            var categoryName = Console.ReadLine()!;
+
+            var result = _productService.CreateProduct(title, price, categoryName);
+            if (result != null)
+            {
+                Console.Clear();
+                Console.WriteLine("Product was created");
+                Console.ReadKey();
+            }
+        }
+
+        public void GetProducts_UI()
+        {
+            Console.Clear();
+
+            var products = _productService.GetProducts();
+            foreach (var product in products)
+            {
+                Console.WriteLine($"{product.Title} - {product.Category.CategoryName} ({product.Price} SEK)");
+                
+            }
+            Console.ReadKey();
+        }
+
+        public void UpdateProduct_UI()
+        {
+            Console.Clear();
+            Console.Write("Enter Product ID: ");
+            var id = int.Parse(Console.ReadLine()!);
+
+            var product = _productService.GetProductById(id);
+            if (product != null)
+            {
+                Console.WriteLine($"{product.Title} - {product.Category.CategoryName} ({product.Price} SEK)");
+                Console.WriteLine();
+
+                Console.Write("New Product Title: ");
+                product.Title = Console.ReadLine()!;
+
+                var newProduct = _productService.UpdateProduct(product);
+                Console.WriteLine($"{product.Title} - {product.Category.CategoryName} ({product.Price} SEK)");
+
+            }
+            else
+            {
+                Console.WriteLine("No product found");   
+            }
+            Console.ReadKey();
+        }
+
+        public void DeleteProduct_UI()
+        {
+            Console.Clear();
+            Console.Write("Enter Product ID: ");
+            var id = int.Parse(Console.ReadLine()!);
+
+            var product = _productService.GetProductById(id);
+            if (product != null)
+            {
+                _productService.DeleteProduct(product.Id);
+                
+                Console.WriteLine("Product was deleted");
+
+            }
+            else
+            {
+                Console.WriteLine("No product found");
+            }
+            Console.ReadKey();
+        }
+
+        //CUSTOMER
+
+        public void CreateCustomer_UI()
+        {
+            Console.Clear();
+            Console.WriteLine("Create Customer");
+
+            Console.Write("First Name: ");
+            var firstName = Console.ReadLine()!;
+
+            Console.Write("Last Name: ");
+            var lastName = Console.ReadLine()!;
+
+            Console.Write("Email: ");
+            var email = Console.ReadLine()!;
+
+            Console.Write("Street Name: ");
+            var streetName = Console.ReadLine()!;
+
+            Console.Write("Postal Code: ");
+            var postalCode = Console.ReadLine()!;
+
+            Console.Write("Last name: ");
+            var city = Console.ReadLine()!;
+
+            Console.Write("Role Name: ");
+            var roleName = Console.ReadLine()!;
+
+
+            var result = _customerService.CreateCustomer(firstName, lastName, email, roleName, streetName, postalCode, city);
+            if (result != null)
+            {
+                Console.Clear();
+                Console.WriteLine("Customer was created");
+                Console.ReadKey();
+            }
+        }
+
+        public void GetCustomer_UI()
+        {
+            Console.Clear();
+
+            var customers = _customerService.GetCustomers();
+            foreach (var customer in customers)
+            {
+                Console.WriteLine($"{customer.FirstName} - {customer.LastName} ({customer.Role.RoleName} SEK)");
+                Console.WriteLine($"{customer.Address.StreetName}, {customer.Address.PostalCode} {customer.Address.City}");
+
+            }
+            Console.ReadKey();
+        }
+
+        public void UpdateCustomer_UI()
+        {
+            Console.Clear();
+            Console.Write("Enter Customer Email: ");
+            var email = Console.ReadLine()!;
+
+            var customer = _customerService.GetCustomerByEmail(email);
+            if (customer != null)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"{customer.FirstName} - {customer.LastName} ({customer.Role.RoleName} SEK)");
+                Console.WriteLine($"{customer.Address.StreetName}, {customer.Address.PostalCode} {customer.Address.City}");
+                Console.WriteLine();
+
+                Console.Write("New Last Name: ");
+                customer.LastName = Console.ReadLine()!;
+
+                var newCustomer = _customerService.UpdateCustomer(customer);
+                Console.WriteLine();
+                Console.WriteLine($"{newCustomer.FirstName} - {newCustomer.LastName} ({newCustomer.Role.RoleName} SEK)");
+                Console.WriteLine($"{newCustomer.Address.StreetName}, {newCustomer.Address.PostalCode} {newCustomer.Address.City}");
+                Console.WriteLine();
+
+            }
+            else
+            {
+                Console.WriteLine("No Customer Found");
+            }
+            Console.ReadKey();
+        }
+
+        public void DeleteCustomer_UI()
+        {
+            Console.Clear();
+            Console.Write("Enter Customer Email: ");
+            var email = Console.ReadLine()!;
+
+            var customer = _customerService.GetCustomerByEmail(email);
+            if (customer != null)
+            {
+                _productService.DeleteProduct(customer.Id);
+                Console.WriteLine("Customer was deleted");
+
+            }
+            else
+            {
+                Console.WriteLine("No Customer found");
+            }
+            Console.ReadKey();
+        }
+
+    }
+
+    
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,86 @@
+﻿using ConsoleApp;
+using ConsoleApp.Contexts;
+using ConsoleApp.Repositories;
+using ConsoleApp.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateDefaultBuilder().ConfigureServices(services =>
+{
+
+    services.AddDbContext<DataContext>(x => x.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\CSharp-Projects\ConsoleApp\ConsoleApp\Data\database.mdf;Integrated Security=True;Connect Timeout=30"));
+
+
+    services.AddScoped<AddressRepository>();
+    services.AddScoped<CategoryRepository>();
+    services.AddScoped<RoleRepository>();
+    services.AddScoped<ProductRepository>();
+    services.AddScoped<CustomerRepository>();
+
+    services.AddScoped<AddressService>();
+    services.AddScoped<CategoryService>();
+    services.AddScoped<RoleService>();
+    services.AddScoped<ProductService>();
+    services.AddScoped<CustomerService>();
+
+    services.AddSingleton<ConsoleUI>();
+
+}).Build();
+
+bool menu = true;
+var consoleUI = builder.Services.GetRequiredService<ConsoleUI>();
+
+
+while (menu)
+{
+
+    Console.WriteLine("MENU OPTIONS");
+    Console.WriteLine($"{"1.",-3} Create new product");
+    Console.WriteLine($"{"2.",-3} Show all products");
+    Console.WriteLine($"{"3.",-3} Update a product");
+    Console.WriteLine($"{"4.",-3} Delete product");
+    Console.WriteLine($"{"5.",-3} Create new customer");
+    Console.WriteLine($"{"6.",-3} Show all customers");
+    Console.WriteLine($"{"7.",-3} Update a customer");
+    Console.WriteLine($"{"8.",-3} Delete Customer");
+    Console.WriteLine($"{"0.",-3} Exit Application");
+    Console.WriteLine();
+    Console.Write("Enter Menu Option: ");
+    var option = Console.ReadLine();
+
+    switch (option)
+    {
+        case "1":
+            consoleUI.CreateProduct_UI();
+            break;
+        case "2":
+            consoleUI.GetProducts_UI();
+            break;
+        case "3":
+            consoleUI.UpdateProduct_UI();
+            break;
+        case "4":
+            consoleUI.DeleteProduct_UI();
+            break;
+        case "5":
+            consoleUI.CreateCustomer_UI();
+            break;
+        case "6":
+            consoleUI.GetCustomer_UI();
+            break;
+        case "7":
+            consoleUI.UpdateCustomer_UI();
+            break;
+        case "8":
+            consoleUI.DeleteCustomer_UI();
+            break;
+        case "0":
+            menu = false;
+            break;
+        default:
+            Console.WriteLine("\nInvalid Option Selected. Please try again");
+            Console.ReadKey();
+            break;
+    }
+}
